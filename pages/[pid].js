5b226adf1375017e86f1168a59f05de3fa2c2efd
dhruv50ae <linkdp2012@gmail.com>
2022-06-16 16:34:03 +0100

    Use getStaticPaths
    GetStatisPaths allows you to have path names for each path you have for an identifier. In this case it's pid since it's the dynamic path for the home page. The keys for each pid is the id

--- pages/[pid].js
+++ pages/[pid].js
@@ -26,4 +26,27 @@ export async function getStaticProps({ params }) {
   };
 }
 
+export async function getStaticPaths() {
+  return {
+    paths: [
+      {
+        params: {
+          pid: "p1",
+        },
+      },
+      {
+        params: {
+          pid: "p2",
+        },
+      },
+      {
+        params: {
+          pid: "p3",
+        },
+      },
+    ],
+    fallback: false,
+  };
+}
+
 export default ProductDetailPage;
