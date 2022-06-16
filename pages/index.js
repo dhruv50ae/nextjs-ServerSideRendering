f9b3fe07ba332f6127331065f637dbe7729fab0e
dhruv50ae <linkdp2012@gmail.com>
2022-06-16 15:17:40 +0100

    Deployment and regeneration
    By adding revalidate in the object as a key and a second based value, the page will be deployed every x seconds if you wanted to keep up to date with new information

--- pages/index.js
+++ pages/index.js
@@ -14,12 +14,12 @@ const HomePage = ({ products }) => {
 export async function getStaticProps() {
   const filePath = path.join(process.cwd(), "data", "data.json");
   const jsonData = await fs.readFile(filePath);
-  console.log(jsonData)
   const data = JSON.parse(jsonData);
   return {
     props: {
       products: data.products,
     },
+    revalidate: 10,
   };
 }
 
