59ece6b09e93ec3faf0fa5b7a7a06765809c2abb
dhruv50ae <linkdp2012@gmail.com>
2022-06-17 14:39:07 +0100

    used SWR

--- pages/lastSales.js
+++ pages/lastSales.js
@@ -1,31 +1,51 @@
 import { useEffect, useState } from "react";
+import useSWR from "swr";
 
 const LastSalesPage = () => {
   const [sales, setSales] = useState();
-  const [loading, setLoading] = useState(false);
+  //   const [loading, setLoading] = useState(false);
+
+  const { data, error } = useSWR(
+    "https://nextjs-prac-default-rtdb.firebaseio.com/sales.json"
+  );
   useEffect(() => {
-    setLoading(true);
-    fetch("https://nextjs-prac-default-rtdb.firebaseio.com/sales.json")
-      .then((res) => res.json())
-      .then((data) => {
-        const transformedSales = [];
-        for (let key in data) {
-          transformedSales.push({
-            id: key,
-            username: data[key].username,
-            volume: data[key].volume,
-          });
-        }
-        setSales(transformedSales);
-        setLoading(false);
-      });
-  }, []);
-  if (loading) {
+    if (data) {
+      let transformedSales = [];
+      for (let key in data) {
+        transformedSales.push({
+          id: key,
+          username: data[key].username,
+          volume: data[key].volume,
+        });
+      }
+      setSales(transformedSales);
+    }
+  }, [data]);
+  //   useEffect(() => {
+  //     setLoading(true);
+  //     fetch("https://nextjs-prac-default-rtdb.firebaseio.com/sales.json")
+  //       .then((res) => res.json())
+  //       .then((data) => {
+  //         const transformedSales = [];
+  //         for (let key in data) {
+  //           transformedSales.push({
+  //             id: key,
+  //             username: data[key].username,
+  //             volume: data[key].volume,
+  //           });
+  //         }
+  //         setSales(transformedSales);
+  //         setLoading(false);
+  //       });
+  //   }, []);
+
+  if (error) {
+    return <p>Failed to load data</p>;
+  }
+  if (!data || !sales) {
     return <p>Loading...</p>;
   }
-  if (!sales) {
-    return <p>No data yet</p>;
-  }
+
   return (
     <ul>
       {sales.map((sale) => {
@@ -33,7 +53,7 @@ const LastSalesPage = () => {
           <li key={sale.id}>
             {sale.usename} - {sale.volume}
           </li>
-        );
+        )
       })}
     </ul>
   );
